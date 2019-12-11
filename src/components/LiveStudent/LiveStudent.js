27f4860f1bf8c5f7d1f3897b85381ffb819a7688
Connor Sullivan <cksullvn@gmail.com>
2019-12-11 14:17:51 -0600

    created extremely rough draft of LiveStudent.js

--- src/components/LiveStudent/LiveStudent.js
+++ src/components/LiveStudent/LiveStudent.js
@@ -1,1 +1,22 @@
 import React from 'react';
+// import PropTypes from 'prop-types';
+import studentShape from '../../helpers/props/studentShape';
+
+class LiveStudent extends React.Component {
+  static propTypes = {
+    students: studentShape.studentShape,
+  }
+
+  render() {
+    const { liveStudent } = this.props;
+    return (
+      <div className="card">
+        <div className="card-body">
+          <h5 className="card-title">{liveStudent.firstName} {liveStudent.lastName}</h5>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default LiveStudent;
