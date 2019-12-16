d3b758cb60d691375d91e26264f6381c45eb6a6a
Connor Sullivan <cksullvn@gmail.com>
2019-12-16 15:20:15 -0600

    styling and readme has been updated

--- src/components/Gravestone/Gravestone.js
+++ src/components/Gravestone/Gravestone.js
@@ -10,10 +10,9 @@ class Gravestone extends React.Component {
   render() {
     const { gravestone } = this.props;
     return (
-      <div className="card">
-        <div className="card-body">
-          <h5 className="card-title">{gravestone.firstName} {gravestone.lastName}</h5>
-        </div>
+      <div className="card-body grave-container">
+        <img id="coffin" src="https://myrealdomain.com/images/clip-art-coffin-5.png" alt="coffin"/>
+        <h5 className="card-title graveText"> {gravestone.firstName} {gravestone.lastName} </h5>
       </div>
     );
   }
